14c2adb4db786ed23d52701e02765567f52dc3e6
Greg Guthe <gguthe@mozilla.com>
2017-11-21 18:08:09 -0500

    Add ping management command

---
+++ relops_hardware_controller/api/management/commands/ping.py
@@ -0,0 +1,62 @@
+
+import re
+import subprocess
+
+from django.core.exceptions import ValidationError
+from django.core.management.base import BaseCommand, CommandError
+from django.core.validators import validate_ipv46_address
+
+
+class Command(BaseCommand):
+    help = 'Tries to ICMP ping the host. Raises for exceptions for a lost packet or timeout.'
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument(
+            'host',
+            type=str,
+            help='A host',
+        )
+
+        # Named (optional) arguments
+        parser.add_argument(
+            '-c',
+            dest='count',
+            default=4,
+            type=int,
+            help='stop after sending NUMBER packets',
+        )
+        parser.add_argument(
+            '-w',
+            dest='timeout',
+            default=5,
+            type=int,
+            help='stop after N seconds',
+        )
+
+    def get_args_and_kwargs_from_job(self, _, machine):
+        args = [machine.get('host', None) or machine.get('ip', None)]
+        kwargs = {}
+        return args, kwargs
+
+    def validate_host(self, host):
+        # from the django URLValidator without unicode
+        hostname_re = r'^[\.a-z0-9](?:[\.a-z0-9-]{0,61}[\.a-z0-9])?$'
+        if not re.match(hostname_re, host):
+            validate_ipv46_address(host)
+
+    def handle(self, host, *args, **options):
+        self.validate_host(host)
+
+        call_args = [
+            'ping',
+            '-c', str(options['count']),
+            '-w', str(options['timeout']),
+            host,
+        ]
+
+        # Raises exceptions for failure, non-zero returncode, and timeouts
+        subprocess.run(
+            call_args,
+            timeout=options['timeout'],
+            check=True)
---
+++ tests/test_ping_command.py
@@ -0,0 +1,57 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+
+import subprocess
+import pytest
+
+from django.core.exceptions import ValidationError
+from django.core.management import (
+    call_command,
+    load_command_class,
+)
+from django.core.management.base import CommandError
+from django.utils.six import StringIO
+
+from relops_hardware_controller.api.serializers import (
+    MachineSerializer,
+)
+from relops_hardware_controller.api.models import (
+    Machine,
+)
+
+
+def test_ping_requires_host_or_machine_id():
+    with pytest.raises(CommandError):
+        call_command('ping')
+
+
+def test_ping_rejects_invalid_host():
+    with pytest.raises(ValidationError):
+        call_command('ping', '; echo foo')
+
+
+@pytest.mark.slowtest
+def test_ping_localhost_works():
+    call_command('ping', 'localhost', count=1, timeout=1)
+
+
+@pytest.mark.slowtest
+def test_ping_unroutable_ip_timesout():
+    with pytest.raises(subprocess.TimeoutExpired):
+        # Note: IP won't necessarily be unroutable
+        call_command('ping', '198.51.100.0', count=1, timeout=1)
+
+
+@pytest.mark.slowtest
+@pytest.mark.django_db
+def test_ping_using_machine_id_works():
+    tc_worker = {}
+    machine = Machine.objects.create(host='localhost', ip='127.0.0.1')
+    machine.save()
+
+    cmd_class = load_command_class('relops_hardware_controller.api', 'ping')
+
+    args, kwargs = cmd_class.get_args_and_kwargs_from_job(tc_worker, MachineSerializer(machine).data)
+
+    call_command(cmd_class, *args, **kwargs)
