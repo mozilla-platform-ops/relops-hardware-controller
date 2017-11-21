1fd3bb1abdc08b7dbf9e496727607efa3cb12650
Greg Guthe <gguthe@mozilla.com>
2017-11-21 18:06:28 -0500

    Add relops_hardware_controller project

---
+++ relops_hardware_controller/__init__.py
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+from .celery import app as celery_app
+
+default_app_config = 'relops_hardware_controller.apps.RelopsHardwareControllerAppConfig'
+
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+__all__ = ['celery_app', 'default_app_config']
---
+++ relops_hardware_controller/apps.py
@@ -0,0 +1,42 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+
+import logging
+from urllib.parse import urlparse
+
+from django_redis import get_redis_connection
+
+from django.conf import settings
+from django.apps import AppConfig
+
+
+logger = logging.getLogger('django')
+
+
+class RelopsHardwareControllerAppConfig(AppConfig):
+    name = 'relops_hardware_controller'
+
+    def ready(self):
+        # The app is now ready.
+
+        # Because shortcomings in django-configurations, we can't set
+        # DATABASES with CONN_MAX_AGE. So let's fix it here.
+        if settings.CONN_MAX_AGE:
+            settings.DATABASES['default']['CONN_MAX_AGE'] = (
+                settings.CONN_MAX_AGE
+            )
+
+        # For some unknown reason, if you don't do at least one read
+        # from the Redis connection before you do your first write,
+        # you can get a `redis.exceptions.ConnectionError` with
+        # "Error 9 while writing to socket. Bad file descriptor."
+        # This is only occuring in running unit tests.
+        # But only do this if the caches['default'] isn't a fake one
+        # redis_client_class = settings.CACHES['default']['OPTIONS'].get(
+        #     'REDIS_CLIENT_CLASS'
+        # )
+        # if redis_client_class != 'fakeredis.FakeStrictRedis':
+        if 'LocMemCache' not in settings.CACHES['default']['BACKEND']:
+            connection = get_redis_connection('default')
+            connection.info()
