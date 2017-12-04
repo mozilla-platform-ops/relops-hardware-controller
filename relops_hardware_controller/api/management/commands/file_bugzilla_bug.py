9298e7dd61bd3aa19e9e3548d25a3b5361a0a088
Greg Guthe <gguthe@mozilla.com>
2017-12-04 18:48:13 -0500

    Add file bugzilla bug command
    fixes #9

---
+++ relops_hardware_controller/api/management/commands/file_bugzilla_bug.py
@@ -0,0 +1,70 @@
+
+import logging
+import json
+import re
+import subprocess
+
+from django.core.exceptions import ValidationError
+from django.core.management.base import BaseCommand, CommandError
+from django.core.validators import validate_ipv46_address
+import requests
+
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Files a new bugzilla bug for the host. Raises for exceptions for bad or invalid responses.'
+    doc_url = 'https://github.com/mozbhearsum/bzrest/blob/master/bzrest/client.py'
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument(
+            'host',
+            type=str,
+            help='A host',
+        )
+
+    def get_args_and_kwargs_from_job(self, _, machine):
+        args = [machine.get('host', None) or machine.get('ip', None)]
+        kwargs = {}
+        return args, kwargs
+
+    def validate_host(self, host):
+        # from the django URLValidator without unicode
+        hostname_re = r'^[\.a-z0-9](?:[\.a-z0-9-]{0,61}[\.a-z0-9])?$'
+        if not re.match(hostname_re, host):
+            validate_ipv46_address(host)
+
+    def handle(self, host, *args, **options):
+        self.validate_host(host)
+
+        bgz_url = settings.BUGZILLA_URL
+        if bgz_url.endswith('/'):
+            url = bgz_url + 'bug'
+        else:
+            url = bgz_url + '/bug'
+
+        response = requests.post(
+            url,
+            json=dict(
+                Bugzilla_api_key=settings.BUGZILLA_API_KEY,
+                product='Infrastructure & Operations',
+                component='DCOps',
+                summary='{} is unreachable'.format(host),
+                version='other',
+                op_sys='All',
+                platform='All',
+                # blocks=  # TODO: add machine bug id for tracking
+            ),
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            }
+        )
+        logger.debug('file bug response: {}'.format(response.content))
+        response.raise_for_status()
+        # will raise errors from invalid JSON too https://github.com/mozbhearsum/bzrest/blob/ae48450dda27ccfe9ac4ced26752fe0bc85770b8/bzrest/client.py#L63-L65
+        bug_id = response.json()["id"]
+        logger.info('created bug {} for {}'.format(bug_id, host))
---
+++ tests/test_file_bugzill_bug.py
@@ -0,0 +1,62 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+
+import json
+import subprocess
+import mock
+import pytest
+
+from django.core.exceptions import ValidationError
+from django.core.management import call_command
+from django.core.management.base import CommandError
+
+
+@pytest.mark.bugzilla
+def test_file_bugzilla_bug_requires_host():
+    with pytest.raises(CommandError):
+        call_command('file_bugzilla_bug')
+
+
+@pytest.mark.bugzilla
+def test_file_bugzilla_bug_rejects_invalid_host():
+    with pytest.raises(ValidationError):
+        call_command('file_bugzilla_bug', '; echo foo')
+
+
+@pytest.mark.bugzilla
+def test_file_bugzilla_bug_localhost_works():
+    with mock.patch('requests.post') as post_mock:
+        post_mock.return_value.json.return_value = {'id': 42}
+        post_mock.return_value.content = 'mock bugzilla response'
+
+        call_command('file_bugzilla_bug', 'localhost')
+
+        assert post_mock.called
+        post_mock.assert_called_once_with(
+            'https://landfill.bugzilla.org/bugzilla-5.0-branch/rest/bug',
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            json={
+                'Bugzilla_api_key': 'anything',
+                'product': 'Infrastructure & Operations',
+                'component': 'DCOps',
+                'summary': 'localhost is unreachable',
+                'version': 'other',
+                'op_sys': 'All',
+                'platform': 'All'
+        })
+
+
+@pytest.mark.bugzilla
+def test_file_bugzilla_bug_error_response():
+    with mock.patch('requests.post') as post_mock:
+        post_mock.return_value.content = 'mock bugzilla response'
+        post_mock.return_value.raise_for_status.side_effect = Exception('Bad Request')
+
+        with pytest.raises(Exception):
+            call_command('file_bugzilla_bug', 'localhost')
+
+        assert post_mock.called
