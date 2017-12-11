7b924a5e1ef59d14ba5826fa72287d18a56963d3
Greg Guthe <gguthe@mozilla.com>
2017-12-11 15:59:50 -0600

    skip flakey ping unroutable ip test

--- tests/test_ping_command.py
+++ tests/test_ping_command.py
@@ -36,10 +36,10 @@ def test_ping_localhost_works():
     call_command('ping', 'localhost', count=1, timeout=1)
 
 
+@pytest.mark.skip(reason='IP isn\'t always unroutable')
 @pytest.mark.requires_worker
 def test_ping_unroutable_ip_timesout():
     with pytest.raises(subprocess.TimeoutExpired):
-        # Note: IP won't necessarily be unroutable
         call_command('ping', '198.51.100.0', count=1, timeout=1)
 
 
