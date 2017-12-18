b8411dd64889a79af981be5e8dc3e3cc3672ec25
Greg Guthe <gguthe@mozilla.com>
2017-12-18 13:41:41 -0500

    update views to not use db

--- tests/test_api_views.py
+++ tests/test_api_views.py
@@ -12,13 +12,6 @@ from django.core.urlresolvers import reverse
 from django.utils.http import urlencode
 
 
-from relops_hardware_controller.api.models import (
-    Job,
-    Machine,
-    TaskClusterWorker,
-)
-
-
 def get_hawk_auth_header(method, url, client_id=None, access_token=None, content_type='application/json'):
     return mohawk.Sender(
         credentials={
@@ -85,91 +78,10 @@ def test_job_list_returns_405_for_authed_get(client):
         assert tc_auth_ctor.called
 
 
-@pytest.mark.django_db
-def test_job_list_returns_404_for_unknown_worker_id(client):
-    query_params = urlencode(dict(task_name='ping'))
-    uri = reverse('api:JobList',
-                  kwargs=dict(worker_id='t-yosemite-r7-313',
-                              worker_group='mdc1')) + '?' + query_params
-
-    host = '127.0.0.1:9091'
-    url = 'http://' + host + uri
-    auth_header = get_hawk_auth_header('POST', url)
-
-    with mock.patch('taskcluster.Auth') as tc_auth_ctor:
-        tc_client = tc_auth_ctor.return_value
-        tc_client.authenticateHawk.return_value = {
-            'scopes': ['project:relops-hardware-controller:ping'],
-            'status': 'auth-success',
-        }
-        response = client.post(url,
-                               HTTP_HOST=host,
-                               HTTP_CONTENT_TYPE='application/json',
-                               HTTP_AUTHORIZATION=auth_header)
-
-        assert response.status_code == 404
-        has_cors_headers(response)
-
-        tc_client.authenticateHawk.assert_called_once_with({
-            'method': 'post',
-            'resource': '/api/v1/workers/t-yosemite-r7-313/group/mdc1/jobs',
-            'host': '127.0.0.1',
-            'port': 80,
-            'authorization': auth_header,
-        })
-        assert tc_auth_ctor.called
+# TODO: test invalid worker ids and groups too?
 
 
-# TODO: test unknown or invalid worker group too?
-
-
-@pytest.mark.django_db
-def test_job_list_returns_404_for_tc_worker_on_machine_we_do_not_manage(client):
-    worker = TaskClusterWorker.objects.create(tc_worker_id='tc-worker-1')
-    worker.save()
-
-    query_params = urlencode(dict(task_name='ping'))
-    uri = reverse('api:JobList',
-                  kwargs=dict(worker_id='tc-worker-1',
-                              worker_group='mdc1')) + '?' + query_params
-
-    host = '127.0.0.1:9091'
-    url = 'http://' + host + uri
-    auth_header = get_hawk_auth_header('POST', url)
-
-    with mock.patch('taskcluster.Auth') as tc_auth_ctor:
-        tc_client = tc_auth_ctor.return_value
-        tc_client.authenticateHawk.return_value = {
-            'scopes': ['project:relops-hardware-controller:ping'],
-            'status': 'auth-success',
-        }
-
-        response = client.post(uri,
-                               HTTP_HOST=host,
-                               HTTP_CONTENT_TYPE='application/json',
-                               HTTP_AUTHORIZATION=auth_header)
-        print(response.content)
-        assert response.status_code == 404
-        has_cors_headers(response)
-
-        tc_client.authenticateHawk.assert_called_once_with({
-            'method': 'post',
-            'resource': '/api/v1/workers/tc-worker-1/group/mdc1/jobs',
-            'host': '127.0.0.1',
-            'port': 80,
-            'authorization': auth_header,
-        })
-        assert tc_auth_ctor.called
-
-
-@pytest.mark.django_db
 def test_job_list_queues_job_for_valid_post(client):
-    worker = TaskClusterWorker.objects.create(tc_worker_id='tc-worker-1')
-    worker.save()
-    machine = Machine.objects.create(host='localhost', ip='127.0.0.1')
-    machine.workers.add(worker)
-    machine.save()
-
     query_params = urlencode(dict(task_name='ping'))
     uri = reverse('api:JobList',
                   kwargs=dict(worker_id='tc-worker-1',
@@ -205,14 +117,7 @@ def test_job_list_queues_job_for_valid_post(client):
         })
         assert tc_auth_ctor.called
 
-        # assert response.json()['status'] == 'PENDING'
 
-        job_id = UUID(response.json()['task_id'])
-        inserted_job = Job.objects.get(pk=job_id)
-        assert inserted_job
-
-
-@pytest.mark.django_db
 def test_job_list_requires_auth_header_for_post(client):
     query_params = urlencode(dict(task_name='ping'))
     uri = reverse('api:JobList',
@@ -238,7 +143,6 @@ def test_job_list_requires_auth_header_for_post(client):
         assert response.json() == {'detail': 'Unauthorized'}
 
 
-@pytest.mark.django_db
 def test_job_list_requires_status_in_auth_response(client):
     query_params = urlencode(dict(task_name='ping'))
     uri = reverse('api:JobList',
@@ -275,7 +179,6 @@ def test_job_list_requires_status_in_auth_response(client):
         assert tc_auth_ctor.called
 
 
-@pytest.mark.django_db
 def test_job_list_returns_403_for_status_failed_in_auth_response(client):
     query_params = urlencode(dict(task_name='ping'))
     uri = reverse('api:JobList',
@@ -313,7 +216,6 @@ def test_job_list_returns_403_for_status_failed_in_auth_response(client):
         assert tc_auth_ctor.called
 
 
-@pytest.mark.django_db
 def test_job_list_returns_403_for_wtf_status_in_auth_response(client):
     query_params = urlencode(dict(task_name='ping'))
     uri = reverse('api:JobList',
@@ -351,14 +253,7 @@ def test_job_list_returns_403_for_wtf_status_in_auth_response(client):
         assert tc_auth_ctor.called
 
 
-@pytest.mark.django_db
 def test_job_list_returns_403_for_no_task_name_scopes(client):
-    worker = TaskClusterWorker.objects.create(tc_worker_id='tc-worker-1')
-    worker.save()
-    machine = Machine.objects.create(host='localhost', ip='127.0.0.1')
-    machine.workers.add(worker)
-    machine.save()
-
     query_params = urlencode(dict(task_name='ping'))
     uri = reverse('api:JobList',
                   kwargs=dict(worker_id='tc-worker-1',
@@ -396,14 +291,7 @@ def test_job_list_returns_403_for_no_task_name_scopes(client):
         assert tc_auth_ctor.called
 
 
-@pytest.mark.django_db
 def test_job_list_returns_403_for_different_task_name_scope(client):
-    worker = TaskClusterWorker.objects.create(tc_worker_id='tc-worker-1')
-    worker.save()
-    machine = Machine.objects.create(host='localhost', ip='127.0.0.1')
-    machine.workers.add(worker)
-    machine.save()
-
     query_params = urlencode(dict(task_name='ping'))
     uri = reverse('api:JobList',
                   kwargs=dict(worker_id='tc-worker-1',
