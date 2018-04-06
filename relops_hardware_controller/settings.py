370ef09abaec625a870e97947cf9fb43b4cee87f
Dave House <dhouse@mozilla.com>
2018-04-06 09:24:06 -0600

    default hostname regex as open

--- relops_hardware_controller/settings.py
+++ relops_hardware_controller/settings.py
@@ -187,7 +187,7 @@ class Base(Configuration, Celery):
         for task_name in TASK_NAMES.value
     ], seq_separator=',', environ_prefix=None)
 
-    VALID_WORKER_ID_REGEX = values.Value('^t-linux64-ms.*', environ_prefix=None)
+    VALID_WORKER_ID_REGEX = values.Value('^.*', environ_prefix=None)
 
     # Worker Settings
 
