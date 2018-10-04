4d12fb9c2d3ca58a315e51cfc3451f9ad2d0bda6
Dave House <dhouse@mozilla.com>
2018-10-04 13:30:33 -0600

    improve down check and logs for failed reboot

--- relops_hardware_controller/api/management/commands/reboot.py
+++ relops_hardware_controller/api/management/commands/reboot.py
@@ -19,11 +19,10 @@ def can_ping(fqdn, count=4, timeout=5):
     ping_cls = load_command_class('relops_hardware_controller.api', 'ping')
 
     try:
-        call_command(ping_cls, fqdn, 'ping', '-c', count, '-w', timeout)
-        logger.debug('pinging %s succeeded', fqdn)
+        stdout = StringIO()
+        call_command(ping_cls, fqdn, 'ping', '-c', count, '-w', timeout, stdout=stdout)
         return True
     except Exception as error:
-        logger.warn('pinging %s failed: %s', fqdn, error)
         return False
 
 
@@ -53,13 +52,15 @@ def wait_for_state(fn, timeout, interval):
 
 def reboot_succeeded(fqdn):
     def is_down():
-        return not can_ping(fqdn, count=1, timeout=2)
+        return not can_ping(fqdn, count=1, timeout=4)
 
     def is_up():
-        return can_ping(fqdn)
+        return can_ping(fqdn, count=1, timeout=60)
 
-    return wait_for_state(is_down, timeout=settings.DOWN_TIMEOUT, interval=1) and \
-        wait_for_state(is_up, timeout=settings.UP_TIMEOUT, interval=5)
+    powered_down = wait_for_state(is_down, timeout=settings.DOWN_TIMEOUT, interval=1)
+    if not powered_down:
+        return False
+    return wait_for_state(is_up, timeout=settings.UP_TIMEOUT, interval=5)
 
 
 class Command(BaseCommand):
@@ -76,8 +77,10 @@ class Command(BaseCommand):
 
     def handle(self, hostname, job_data, *args, **options):
         start = time.time()
-        result_template = '{command}: {stdout}. Completed in {time:.3g} seconds'
+        rebooted = False
+        result_template = '{command}: {stdout} Completed in {time:.3g} seconds'
         reboot_attempt_log = '\\n'
+        reboot_attempt_log_short = ' '
         config = settings.WORKER_CONFIG
         try:
             server = config['servers'][hostname.split('.')[0]]
@@ -86,7 +89,7 @@ class Command(BaseCommand):
 
         logger.debug('reboot_methods:{}'.format(settings.REBOOT_METHODS))
         stdout = StringIO()
-        for reboot_method in settings.REBOOT_METHODS:
+        for reboot_method in ['ipmi_reset', 'file_bugzilla_bug']: #settings.REBOOT_METHODS:
             reboot_args = []
             logger.debug('reboot_method:{}'.format(reboot_method))
             check = reboot_succeeded
@@ -102,6 +105,9 @@ class Command(BaseCommand):
                             '-l', 'roller',
                             '-i', 'ssh.key',
                         ]
+                elif reboot_method == 'ipmi_on':
+                    reboot_args = [ reboot_method ]
+                    reboot_method = 'ipmi'
                 elif reboot_method == 'ipmi_reset':
                     reboot_args = [ reboot_method ]
                     reboot_method = 'ipmi'
@@ -109,6 +115,8 @@ class Command(BaseCommand):
                     reboot_args = [ reboot_method ]
                     reboot_method = 'ipmi'
                 elif reboot_method == 'snmp_reboot':
+                    if not hasattr(server, 'pdu'):
+                        continue
                     reboot_args = server['pdu'].rsplit(':', 1)
                 elif reboot_method == 'xenapi_reboot':
                     reboot_args = server['xen']['reboot']
@@ -120,7 +128,9 @@ class Command(BaseCommand):
                         json.dumps(job_data),
                         '--log', reboot_attempt_log,
                     ]
-                    check = logger.info
+                    def check(hostname):
+                        logger.info(hostname)
+                        return True
                 else:
                     raise NotImplementedError()
 
@@ -130,7 +140,10 @@ class Command(BaseCommand):
                              stdout=stdout)
 
                 if check(hostname):
+                    rebooted = True
                     break
+                else:
+                    raise Exception('Reboot did not cycle power.')
 
             except Exception as e:
                 logger.exception(e)
@@ -139,6 +152,16 @@ class Command(BaseCommand):
                     reboot_method,
                     ' '.join(reboot_args),
                     e.__class__.__name__)
+                reboot_attempt_log_short += '{} {} {}. '.format(
+                    datetime.utcnow().strftime("%H:%M:%S"),
+                    reboot_method,
+                    e.__class__.__name__)
+
+        if not rebooted:
+            raise Exception('failed:{}'.format(reboot_attempt_log_short))
 
         elapsed = time.time() - start
-        return result_template.format(command=reboot_method, stdout=stdout.getvalue().rstrip('\n'), time=elapsed)
+        return result_template.format(
+            command=' '.join([reboot_method]+reboot_args),
+            stdout=stdout.getvalue().rstrip('\n'),
+            time=elapsed)
